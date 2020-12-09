9e708dc92ac95e99702a73c6ad9f2fe3b9061b09
Vicky <1830118180@qq.com>
2020-12-09 15:01:39 +0800

    完成参与人页面

--- src/pages/Participant.vue
+++ src/pages/Participant.vue
@@ -1,25 +1,53 @@
 <template>
 	<div class="participant">
-		<div class="title">
-			<i class="el-icon-arrow-left"></i>
-			参与人
-		</div>
+		<HeaderBar :titleName="titleName"></HeaderBar>
 		<div class="part-info">
-			<div class="user">
-				<div class="h-img">
-					<img src="../assets/999.jpg" alt="">
+			<div v-show="true" class="user">
+				<div class="user-info">
+					<div class="h-img">
+						<img src="../assets/999.jpg" alt="">
+					</div>
+					<div class="info">
+						<div class="name">
+							<span>开心麻花</span>
+							<i class="el-icon-male">18</i>
+						</div>
+						<div class="type">投资人</div>
+					</div>
 				</div>
-				
 				<div class="detail">
-					<p>出价：80</p>
+					<p class="sty">出价：80</p>
 					<p>2小时前</p>
 				</div>
 			</div>
+			<div v-show="false" class="no-part">
+				<i class="el-icon-warning-outline" style="font-size: 21vw;"></i>
+				<p>暂无参与人</p>
+			</div>
+		</div>
+		<div v-show="true" class="p-bottom">
+			<p>无合适参与人，放弃餐遇</p>
 		</div>
 	</div>
 </template>
 
 <script>
+	import HeaderBar from '../components/HeaderBar.vue';
+	export default {
+		data () {
+			return{
+				'titleName':'参与人',
+			}
+		},
+		components:{
+			HeaderBar
+		},
+		methods:{
+			back () {
+				this.$router.go(-1)
+			}
+ 		}
+	}
 </script>
 
 <style lang='less'>
@@ -29,22 +57,10 @@
 		width: 100%;
 		background-color: #F1F1F1;
 		min-height: 100vh;
-		.title {
-			background: #ffffff;
-			width: 100%;
-			position: fixed;
-			top: 0;
-			text-align: center;
-			font-size: 5.6vw;
-			padding: 3vw 0;
-			font-weight: 600;
-			.el-icon-arrow-left{
-				position: absolute;
-				left:5vw;
-				top: 4.5vw;
-				font-weight: 600;
-			}
-		}
+		p {
+			margin: 0;
+			padding: 0;
+		} 
 		.part-info {
 			padding: 14vw 3vw 0 3vw;
 			.user{
@@ -59,15 +75,72 @@
 				padding: 4vw;
 				margin: 4vw 0;
 				box-sizing: border-box;
-				.h-img {
-					width: 16vw;
-					height: 16vw;
-					overflow: hidden;
-					border-radius: 50%;
-					img {
-						width: 100%;
+				font-size: 4vw;
+				color: @unpick-color;
+				.user-info{
+					display: flex;
+					.h-img {
+						width: 16vw;
+						height: 16vw;
+						overflow: hidden;
+						border-radius: 50%;
+						img {
+							width: 100%;
+						}
+					}
+					.info {
+						padding: 1.5vw 0;
+						display: flex;
+						flex-direction: column;
+						justify-content: space-between;
+						margin-left: 5vw;
+						.name {
+							font-weight: 500;
+							color: #333333;
+							.el-icon-male,el-icom.female{
+								padding: 0.5vw;
+								background-color: #3182FD;
+								color: #FFFFFF;
+								border-radius: 1.5vw;
+								margin-left: 2vw;
+							}
+							.el-icom.female {
+								background-color: #FE6491;
+							}
+						}
 					}
 				}
+				.detail {
+					padding: 1.5vw 0;
+					text-align: right;
+					.sty {
+						color: @base-color;
+					}
+				}
+			}
+			.no-part {
+				margin-top: 50vw;
+				color: @base-color;
+				text-align: center;
+				font-size: 4vw;
+				p {
+					margin-top: 4vw;
+				}
+			}
+		}
+		.p-bottom {
+			position: fixed;
+			height: 15vw;
+			width: 100%;
+			bottom: 0;
+			text-align: center;
+			background-color: #FFFFFF;
+			color: @base-color;
+			display: table;
+			font-size: 4.3vw;
+			p {
+				vertical-align: middle;
+				display: table-cell;
 			}
 		}
 	}
---
+++ src/pages/Publish.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="publish">
+		<div class="p-header">
+			发布餐遇
+			<i class="el-icon-question"></i>
+		</div>
+		<div class="p-cont">
+			<el-carousel :interval="4000" type="card" height="200px">
+				<el-carousel-item v-for="item in 3" :key="item">
+				<h3 class="medium">{{ item }}</h3>
+				</el-carousel-item>
+			</el-carousel>
+		</div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style lang="less">
+	@import '../css/global.less';
+	
+	.el-carousel__item h3 {
+	    color: #475669;
+	    font-size: 14px;
+	    opacity: 0.75;
+	    line-height: 200px;
+	    margin: 0;
+	  }
+	  
+	  .el-carousel__item:nth-child(2n) {
+	    background-color: #99a9bf;
+	  }
+	  
+	  .el-carousel__item:nth-child(2n+1) {
+	    background-color: #d3dce6;
+	  }
+	
+	.publish {
+		background-color: #999999;
+		height: 100vh;
+		.p-header {
+			line-height: 15vw;
+			width: 100%;
+			background-color: #FFFFFF;
+			text-align: center;
+			vertical-align: middle;
+			display: inline-block;
+			font-size: 4.5vw;
+			color: @base-color;
+			position: fixed;
+			.el-icon-question {
+				position: absolute;
+				right: 5vw;
+				top: 5vw;
+				font-size: 5.5vw;
+			}
+		}
+		.p-cont {
+			padding-top: 15vw;
+		}
+	}
+</style>
